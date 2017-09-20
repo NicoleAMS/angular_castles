88ced986e423662c14736d048ffc0d0524b20bd6
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-09-20 16:16:00 +0100

    authentication part 1

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -19,57 +19,61 @@ export class AuthService {
     }
 
     signupUser(email: string, password: string) {
-        firebase.auth().createUserWithEmailAndPassword(email, password)
+        // firebase.auth().createUserWithEmailAndPassword(email, password)
+        this.afAuth.auth.createUserWithEmailAndPassword(email, password)
         .then(
             response => {
-                response.getIdToken().then((token: string) => {
-                    console.log(response);
-                    this.token = token;
-                    return this.http.post('https://angular-castles.firebaseio.com/users.json?auth=' + this.token, response.email);
-                });
+                // response.getIdToken().then((token: string) => {
+                //     console.log(response);
+                //     this.token = token;
+                //     return this.http.post('https://angular-castles.firebaseio.com/users.json?auth=' + this.token, response.email);
+                // });
+                console.log('Successfully signed up!', response);
                 this.signinUser(email, password);
             }
         )
         .catch(
-            error => console.log(error)
+            error => console.log('Signing up went wrong!', error)
         );
     }
 
     signinUser(email: string, password: string) {
-        firebase.auth().signInWithEmailAndPassword(email, password)
+        // firebase.auth().signInWithEmailAndPassword(email, password)
+        this.afAuth.auth.signInWithEmailAndPassword(email, password)
         .then(
             response => {
-                // console.log(response);
+                console.log('Successfully logged in!', response);
                 this.alertMessage = true;
                 // console.log(this.alertMessage);
                 this.welcomeMessage = 'Welcome, ' + email;
                 this.router.navigate(['/']);
-                const currentUser = firebase.auth().currentUser;
-                if (currentUser.email === 'fred@hotmail.com') {
-                    this.isAdmin = true;
-                }
-                currentUser.getIdToken()
-                .then(
-                    (token: string) => {
-                        this.token = token;
-                        console.log(this.token);
-                        localStorage.setItem('savedToken', this.token);
-                    }
-                );
+                // const currentUser = firebase.auth().currentUser;
+                // if (currentUser.email === 'fred@hotmail.com') {
+                //     this.isAdmin = true;
+                // }
+                // currentUser.getIdToken()
+                // .then(
+                //     (token: string) => {
+                //         this.token = token;
+                //         console.log(this.token);
+                //         localStorage.setItem('savedToken', this.token);
+                //     }
+                // );
             }
         )
         .catch(
-            error => console.log(error)
+            error => console.log('Something went wrong while logging in!', error)
         );
 
     }
 
     signout() {
-        firebase.auth().signOut();
+        // firebase.auth().signOut();
+        this.afAuth.auth.signOut();
         this.alertMessage = true;
         this.logoutMessage = 'Successfully logged out';
         // console.log(this.logoutMessage);
-        this.token = null;
+        // this.token = null;
         this.router.navigate(['/']);
     }
 
